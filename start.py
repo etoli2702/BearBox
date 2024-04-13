84d3fe87ae6c9f06edec1f0c09399dcad170ecf5
etoli2702 <123705864+etoli2702@users.noreply.github.com>
2024-04-13 17:36:50 -0500

    added start button

--- start.py
+++ start.py
@@ -1,5 +1,5 @@
 import pygame
-from bear import getScreen
+from bear import getScreen, getWindowSize
 import warnings
 
 # Suppress libpng warning
@@ -9,28 +9,33 @@ class Title:
     def __init__(self):
         self.visible = True
         self.menu = "assets/white.png"
-        self.button = "assets/box_1/box_1.png"
-        self.press = "assets/box_1/box_1_damage.png"
+        self.button = "assets/start.png"
+        self.press = "assets/start2.png"
         self.isPress = False
         self.confirm = False
-        self.location = [960,865,1080,930]
+        self.location = [getWindowSize()[0] / 2.43,getWindowSize()[1] / 1.2,getWindowSize()[0] / 1.78,getWindowSize()[0] / 1.1]
     
     def checkPress(self):
         mouse = pygame.mouse.get_pos()
-        #print(mouse)
+        print(mouse)
         for event in pygame.event.get():
             if (event.type == pygame.MOUSEBUTTONDOWN):
                 if (self.location[0] < mouse[0] < self.location[2] and self.location[1] < mouse[1] < self.location[3]):
                     self.isPress = True
-            elif(event.type != pygame.MOUSEMOTION and self.isPress):
+            elif(event.type == pygame.MOUSEMOTION):
+                if (self.location[0] > mouse[0] or mouse[0] > self.location[2] 
+                    or self.location[1] > mouse[1] or mouse[1] > self.location[3]):
+                    self.isPress = False    
+            elif(self.isPress):
                 self.isPress = False
                 self.confirm = True
+
                 
 
 
     def render(self):
         screen = getScreen()
-        windowSize = pygame.display.get_window_size()
+        windowSize = getWindowSize()
         screen.blit(pygame.image.load("assets/background.png"), (0,0))
         
         if (self.isPress):
@@ -39,8 +44,8 @@ class Title:
         else:
             p = pygame.image.load(self.button)
             
-        p = pygame.transform.scale(p, (windowSize[0] / 16, windowSize[1] / 16))
-        screen.blit(p, (windowSize[0] / 2, windowSize[1] / 1.25))
+        p = pygame.transform.scale(p, (windowSize[0] / 4, windowSize[1] / 3))
+        screen.blit(p, (windowSize[0] / 2.75, windowSize[1] / 1.5))
 
 
             
