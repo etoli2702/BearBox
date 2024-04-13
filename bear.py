cc6df3849136395c1ef6cf2bdfb44446dbb00cba
etoli2702 <123705864+etoli2702@users.noreply.github.com>
2024-04-13 11:51:27 -0500

    update

--- bear.py
+++ bear.py
@@ -3,6 +3,7 @@ import sys
 from InputHandler import InputHandler
 from box import Box
  
+windowSize = [800,600]
 # define a main function
 def main():
     game = BearBox()
@@ -20,10 +21,18 @@ class BearBox:
         pygame.display.set_icon(logo)
         pygame.display.set_caption("test")
         # load and set the logo
+ 
+
+        self.screen = pygame.display.set_mode((windowSize[0], windowSize[1]))
         
+<<<<<<< HEAD
+        # define a variable to control the main loop
+        running = True
+=======
         # create a surface on screen that has the size of 240 x 180
         self.screen = pygame.display.set_mode((800, 600))
         
+>>>>>>> 474b0f3ef28a1a6da7d542a4f0e031e7b556aa3e
         self.activeBox = Box()
         self.activeBox.action()
 
@@ -44,15 +53,32 @@ class BearBox:
             if not (clickPostion is None):
                 print(f"Player clicked at {clickPostion}")
 
-            self.render()
+            self.render(self.activeBox)
 
+<<<<<<< HEAD
+    def render(self, box):
+        
+=======
         pygame.quit()
 
     def render(self):
+>>>>>>> 474b0f3ef28a1a6da7d542a4f0e031e7b556aa3e
         self.screen.blit(pygame.image.load("assets/white.png"), (0,0))
+        p = pygame.image.load(box.image)
+        p = p = pygame.transform.scale(p, (windowSize[0] / 2, windowSize[1] / 2))
+        self.screen.blit(p, (windowSize[0] / 4, windowSize[1] / 4))
+        q = pygame.image.load(box.latchLeft)
+        q = pygame.transform.scale(q, (windowSize[0] / 16, windowSize[1] / 5))
+        self.screen.blit(q, (windowSize[0] / 3, windowSize[1] / 4))
+        r = pygame.image.load(box.latchRight)
+        r = pygame.transform.scale(r, (windowSize[0] / 16, windowSize[1] / 5))
+        self.screen.blit(r, (windowSize[0] / 1.65, windowSize[1] / 4))
         pygame.display.flip()
 
+<<<<<<< HEAD
+=======
         # self.activeBox.render()
+>>>>>>> 474b0f3ef28a1a6da7d542a4f0e031e7b556aa3e
 
     @staticmethod
     def shouldQuit():
