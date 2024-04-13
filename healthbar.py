ff5c22808bbab09e6b7bc4fbeaa5b328b0d20efa
Sasha5049 <asharman@ku.edu>
2024-04-13 13:07:54 -0500

    add render function

--- healthbar.py
+++ healthbar.py
@@ -6,18 +6,24 @@ Last modified: 04/13/2024
 Purpose: Render the healthbar.
 '''
 import pygame
+from bear import getScreen, getWindowSize
 
 class Healthbar:
     def __init__(self):
         pass
     def current_health(self, global_health):
+        """Return a list of assests that need to be rendered in the healthbar."""
         healthbar = list()
         for num in range(8, global_health // 10, -1):
-            print(num)
-            healthbar.append(f"healthbar/food_{num}.png")
+            healthbar.append(f"assests/healthbar/food_{num}.png")
         return(healthbar)
         
-from healthbar import Healthbar
-health = Healthbar()
-print(health.current_health(0))
-        
+    def render(self, global_health):
+        # screen = getScreen()
+        # windowSize = getWindowSize()
+        healthbar = self.current_health(global_health)
+        for item, index in enumerate(healthbar):
+            r = pygame.image.load(item, (50 + index * 100, 50))
+           # r = pygame.transform.scale(r, (windowSize[0] / 3, windowSize[1] / 3))
+           # self.screen.blit(r, (windowSize[0] / 4, windowSize[1] / 4))
+           # pygame.display.flip()
